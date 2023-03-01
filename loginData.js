f375d664d9f0fa9f4d750415f8d3a9a794cc56e8
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2023-03-01 20:59:06 +0700

    add validate

---
+++ loginData.js
@@ -0,0 +1,18 @@
+var login = new Vue({
+    el: '#login',
+    data: {
+        username: '',
+        password: ''
+
+        },
+    methods: {
+        checkLogin() {
+            if(this.username == "" || this.password == ""){
+                alert('Please enter all inputs')
+                return
+            }
+            console.log("yes")
+            window.location.replace("./Dashboard.html");
+        }
+    }
+})
