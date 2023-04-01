7c8a34ecf1757a0319b9380d71fb76338cf434f0
MindNara <64070171@it.kmitl.ac.th>
2023-04-01 15:29:49 +0700

    update connect database

--- routes/Dashboard.js
+++ routes/Dashboard.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const pool = require("../config");
 const router = express.Router();
 
-router.get('/Dashboard', function (req, res) {
+router.get('/Dashboard', async function (req, res) {
     console.log("Hello")
-    res.render('Dashboard')
+
+    const [rows, fields] = await pool.query("SELECT * FROM user WHERE user_id = 1");
+    console.log(rows)
+
+    res.render('Dashboard', {
+        user: rows[0]
+    })
 })
 
 exports.router = router;
