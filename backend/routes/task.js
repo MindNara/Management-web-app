4e77ccff007b046b5ad21f5c328389399c21d204
MindNara <64070171@it.kmitl.ac.th>
2023-04-08 18:29:21 +0700

    edit date on task page

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/Task", async function (req, res, next) {
     try {
-        const [task, fields] = await pool.query("SELECT * FROM to_do_list JOIN user USING(user_id) WHERE user.user_id = 1");
+        const [task, fields] = await pool.query("SELECT list_id, DATE_FORMAT(list_date, '%Y-%m-%d') as list_create_date, DATE_FORMAT(list_date, '%Y-%m-%d') as list_date, list_act, list_status, user_id FROM to_do_list JOIN user USING(user_id) WHERE user.user_id = 1");
 
         res.json({
             task: task,
