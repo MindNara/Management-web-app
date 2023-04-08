a8013130b15d0b928f2bc0a573c7906a2695bfe4
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2023-04-08 16:53:13 +0700

    update json

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const pool = require("../backend/config");
+const pool = require("../config");
 const router = express.Router();
 
 router.get("/Task", async function (req, res, next) {
     try {
         const [task, fields] = await pool.query("SELECT * FROM to_do_list WHERE user_id = 1");
-        return res.json(task);
+        const [user, fields2] = await pool.query("SELECT * FROM user WHERE user_id = 1");
+
+        res.json({
+            task: task,
+            user: user[0]
+        })
     } 
     catch (err) {
         return next(err)
