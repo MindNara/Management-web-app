586d5c1acadadd82cb4bac9d3b84607beb72617c
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2023-05-13 14:41:14 +0700

    last

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <!-- <router-view :key="$route.fullPath" /> -->
-  <router-view :key="$route.fullPath" @auth-change="onAuthChange" :user="user" />
+  <router-view :key="$route.fullPath" />
+  <!-- <router-view :key="$route.fullPath" @auth-change="onAuthChange" :user="user" /> -->
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 
 // export default {
 //   name: "App",
@@ -17,26 +17,26 @@ export default {
       user: null,
     };
   },
-  mounted() {
-    this.onAuthChange();
-  },
-  methods: {
-    onAuthChange() {
-      const token = localStorage.getItem("token");
-      if (token) {
-        this.getUser();
-      }
-    },
-    getUser() {
-      const token = localStorage.getItem("token");
-      axios
-        .get("http://localhost:3000/user/me", {
-          headers: { Authorization: "Bearer " + token },
-        })
-        .then((res) => {
-          this.user = res.data;
-        });
-    },
-  },
+  // mounted() {
+  //   this.onAuthChange();
+  // },
+  // methods: {
+  //   onAuthChange() {
+  //     const token = localStorage.getItem("token");
+  //     if (token) {
+  //       this.getUser();
+  //     }
+  //   },
+  //   getUser() {
+  //     const token = localStorage.getItem("token");
+  //     axios
+  //       .get("http://localhost:3000/user/me", {
+  //         headers: { Authorization: "Bearer " + token },
+  //       })
+  //       .then((res) => {
+  //         this.user = res.data;
+  //       });
+  //   },
+  // },
 };
 </script>
