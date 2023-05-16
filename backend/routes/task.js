31e003876cb889e53f6b62a330401e0146349a89
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2023-05-16 11:12:37 +0700

    addNewTask

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const pool = require("../config");
 const router = express.Router();
-// const Joi = require('joi');
+const Joi = require('joi');
 
-// const schemaInsert = Joi.object({ // สร้าง Joi มา check data ที่เข้ามาว่าครบยัง
-//     list_date: Joi.date().required(),
-//     list_act: Joi.string().required(),
-// }).unknown() // ถ้าไม่ใส่มันจะ error เพราะใน body มี due_date แต่เราไม่ได้ check ในนี้อะเลยต้องใส่ไว้
+const schemaInsert = Joi.object({ // สร้าง Joi มา check data ที่เข้ามาว่าครบยัง
+    list_date: Joi.date().required(),
+    list_act: Joi.string().required(),
+})
 
 router.get("/Task/:userId", async function (req, res, next) {
     try {
@@ -22,36 +22,38 @@ router.get("/Task/:userId", async function (req, res, next) {
 });
 
 
-// router.post("/Task/add", async (req, res, next) => {
-//     try {
-//         await schemaInsert.validateAsync(req.body, { abortEarly: false}) // เอา joi ที่เราสร้างไว้มาเช็ค data ที่ได้มาจาก req.body บางครั้งมาจาก query ก็ต้องใช้ req.query
-//     } catch (error) {
-//         return res.status(400).send(error)
-//     }
+router.post("/Task/add/:userId", async (req, res, next) => {
+    try {
+        await schemaInsert.validateAsync(req.body, { abortEarly: false}) // เอา joi ที่เราสร้างไว้มาเช็ค data ที่ได้มาจาก req.body บางครั้งมาจาก query ก็ต้องใช้ req.query
+    } catch (error) {
+        return res.status(400).send(error)
+    }
 
-//     const conn = await pool.getConnection() // สร้าง transaction ก่อนจะไปทำการลบ
-//     await conn.beginTransaction();
+    const conn = await pool.getConnection() // สร้าง transaction ก่อนจะไปทำการลบ
+    await conn.beginTransaction();
 
-//     const list_act = req.body.list_act
-//     const list_date = req.body.list_date
+    const user_id = req.params.userId
+    const list_act = req.body.list_act
+    const list_date = req.body.list_date
 
-//     console.log(list_act)
-//     console.log(list_date)
+    console.log(user_id)
+    console.log(list_act)
+    console.log(list_date)
 
-//     try {
-//         const [data] = await pool.query('INSERT INTO to_do_list(list_create_date, list_date, list_act, list_status, user_id) VALUES (CURRENT_TIMESTAMP, ?, ?, 0, 1);', 
-//         [list_date, list_act])
+    try {
+        const [data] = await pool.query('INSERT INTO to_do_list(list_create_date, list_date, list_act, list_status, user_id) VALUES (CURRENT_TIMESTAMP, ?, ?, 0, ?);', 
+        [list_date, list_act, user_id])
 
-//         await conn.commit()
-//         res.send({
-//             "message": `"สร้าง ToDo สำเร็จ"`,
-//         })
-//     } catch (error) {
-//         await conn.rollback()
-//         res.send(error)
-//     }finally{
-//         conn.release()
-//     }
-// })
+        await conn.commit()
+        res.send({
+            "message": `"สร้าง ToDo สำเร็จ"`,
+        })
+    } catch (error) {
+        await conn.rollback()
+        res.send(error)
+    }finally{
+        conn.release()
+    }
+})
 
 exports.router = router;
